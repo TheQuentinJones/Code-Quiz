e7455ac1b41e5d7a3f17b9080d2ad27a510fd465
NEO <qj82189@gmail.com>
2022-12-30 10:37:28 -0500

    started pseudocode

---
+++ script.js
@@ -0,0 +1,25 @@
+// Add event listeners for each button "click"
+
+// Event "click" will trigger next questions
+
+// Hide previous question once answered (event listener)
+
+// add display: none or display: block (for correct or incorrect responses)
+
+// i.e class selector to modify for display none
+
+
+
+// maybe write questions in JavaScript?
+
+
+
+// if function for subtraction of time if incorrect answer selected + to incorrect answer tally
+
+// correct answer -->  no time penalty + correct answer tally
+
+
+// code for input in html to save score and input initials
+
+// change styling using JS when switching questions
+
