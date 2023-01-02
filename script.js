ba99e621530cb17d566c3060268b2c83b46ef0fe
NEO <qj82189@gmail.com>
2023-01-02 13:17:45 -0500

    Added some JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// Time Interval
+
+var timeLeft = document.querySelector("#time-left");
+
+var secondsLeft = 60;
+
+function setTime() {
+
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeLeft.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+
+
+            clearInterval(timerInterval);
+
+            sendMessage();
+        }
+    }, 1000);
+
+
+}
+
+function sendMessage() {
+
+    timeLeft.textContent = "Game Over! Try again? ";
+}
+
+// setTime()
+
+var startGame = document.querySelector("#start-game")
+
+startGame.addEventListener("click", setTime())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Add event listeners for each button "click"
 
 // Event "click" will trigger next questions
